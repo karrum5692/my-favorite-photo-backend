8289cb3d7583ada79705cf7333d658f61f9cef2d
karrum5692 <ravee569078@gmail.com>
2026-06-02 09:14:57 +0900

    chore: port 세팅 변경

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
   console.log(`서버 실행 중: http://localhost:${PORT}`);
