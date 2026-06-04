62a6d8644c39b35bae43fe051837a742e42159f0
김상우 <kksw7000@gmail.com>
2026-06-04 10:23:45 +0900

    feat: 회원가입 로그인

--- src/app.js
+++ src/app.js
@@ -1,6 +1,13 @@
+import 'dotenv/config';
 import express from 'express';
 import cors from 'cors';
 import tradeRouter from './modules/trade/trade.router.js';
+
+import authRouter from './modules/auth/auth.router.js';
+// import userRouter from './modules/user/user.router.js';
+// import marketplaceRouter from './modules/marketplace/marketplace.router.js';
+// import pointRouter from './modules/point/point.router.js';
+// import notificationRouter from './modules/notification/notification.router.js';
 import errorMiddleware from './middlewares/error.middleware.js';
 
 const app = express();
@@ -9,6 +16,12 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/market', tradeRouter);
+// 라우터 등록
+app.use('/auth', authRouter);
+// app.use('/users', userRouter);
+// app.use('/market', marketplaceRouter);
+// app.use('/rewards', pointRouter);
+// app.use('/notifications', notificationRouter);
 
 app.get('/', (req, res) => {
   res.json({ message: '안녕하세요' });
