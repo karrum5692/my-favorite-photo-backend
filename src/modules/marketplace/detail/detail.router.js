53ade9f06d434be32533586b742cd5f11de8eb2f
apple <apple@apples-MacBook-Air.local>
2026-06-05 11:37:17 +0900

    feat: 카드 상세 페이지 API 구현

---
+++ src/modules/marketplace/detail/detail.router.js
@@ -0,0 +1,16 @@
+import { Router } from 'express';
+import detailController from './detail.controller.js';
+import authMiddleware from '../../../middlewares/auth.middleware.js';
+
+const router = Router();
+
+router.get('/cards/:id', detailController.getDetailCard);
+router.post(
+  '/cards/:id/purchase',
+  authMiddleware,
+  detailController.purchaseCard
+);
+router.patch('/cards/:id', detailController.patchedCard);
+router.delete('/cards/:id', detailController.cancelledCard);
+
+export default router;
